48b193acf04791dcc9bf6a17dbe4c5d9ad5d1ddc
liuhong <liuhong@creality.com>
2021-09-16 19:30:34 +0800

    Drolon (drolon@infonie.fr)

--- FreeImage/PixelAccess.cpp
+++ FreeImage/PixelAccess.cpp
@@ -3,7 +3,6 @@
 //
 // Design and implementation by
 // - Floris van den Berg (flvdberg@wxs.nl)
-// - Hervé Drolon (drolon@infonie.fr)
 // - Ryan Rubley (ryan@lostreality.org)
 // - Riley McNiff (rmcniff@marexgroup.com)
 //
